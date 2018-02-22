4125e0585b60408089c7b68bbe6334bfcb2b486b
dpeletz <dpeletz@macalester.edu>
2018-02-22 17:58:43 -0600

    added queue page

---
+++ src/pages/queue/queue.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-queue',
+  templateUrl: 'queue.html'
+})
+export class QueuePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
