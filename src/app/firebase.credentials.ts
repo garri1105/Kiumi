100a347d53a38cf39d8690a01a7db978a308d985
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-01 11:57:38 -0600

    Added firebase to application

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBnL7vJC0XmK99CUMo2q1AzqldkvFX0114",
+  authDomain: "traffionic.firebaseapp.com",
+  databaseURL: "https://traffionic.firebaseio.com",
+  projectId: "traffionic",
+  storageBucket: "",
+  messagingSenderId: "48365895185"
+};
