0bd0ec506ad36dfea0494f3d89359d34b2316114
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-04 20:03:33 -0600

    CourseList now accepts nextPage as parameter

--- src/pages/student-check-in/student-check-in.ts
+++ src/pages/student-check-in/student-check-in.ts
@@ -35,8 +35,6 @@ export class StudentCheckInPage {
 
       var distance = countDownDate - now;
 
-      console.log(distance);
-
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
