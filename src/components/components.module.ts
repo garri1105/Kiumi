e4c11cc12b11940ba78139e238a08ac20687d955
razakhalid <rkhalid@macalester.edu>
2018-03-04 19:58:17 -0600

    Merge branch 'master' of https://github.com/sahmed1997/TraffIonic
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StudentQueueComponent } from './student-queue/student-queue';
+<<<<<<< HEAD
+import { OfficeHourListComponent } from './office-hour-list/office-hour-list';
+
+@NgModule({
+	declarations: [StudentQueueComponent,
+    OfficeHourListComponent],
+	imports: [],
+	exports: [StudentQueueComponent,
+    OfficeHourListComponent]
+=======
 import { CourseListComponent } from './course-list/course-list';
 
 @NgModule({
@@ -8,5 +18,6 @@ import { CourseListComponent } from './course-list/course-list';
 	imports: [],
 	exports: [StudentQueueComponent,
     CourseListComponent]
+>>>>>>> e3a4a5387c14364ffda69629d588e8c361b9d38e
 })
 export class ComponentsModule {}
