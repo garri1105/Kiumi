97d3134d7d5faaa507baddcde80f175ced905cee
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-18 19:14:19 -0500

    Added search function to add courses to your profile

---
+++ src/components/course-search/course-search.ts
@@ -0,0 +1,59 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Course} from "../../models/course/course.interface";
+import {CourseDataProvider} from "../../providers/course-data/course-data";
+import {ProfileDataProvider} from "../../providers/profile-data/profile-data";
+import {Profile} from "../../models/profile/profile.interface";
+import {User} from "firebase/app";
+import {AuthProvider} from "../../providers/auth/auth";
+
+/**
+ * Generated class for the CourseSearchComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'course-search',
+  templateUrl: 'course-search.html'
+})
+export class CourseSearchComponent {
+
+  courseList: Course[];
+  selectedCourses: Course[] = [];
+
+  @Input() profile: Profile;
+  @Output() addedCourses: EventEmitter<Course[]>;
+
+  constructor(private courseData: CourseDataProvider) {
+    this.addedCourses = new EventEmitter<Course[]>();
+  }
+
+  searchCourse(query: string) {
+    this.courseList = this.selectedCourses;
+
+    if (query.length > 0) {
+      this.courseData.searchCourse(query)
+        .subscribe(courses => courses.valueChanges()
+          .subscribe(list => {
+            this.courseList = this.courseList.concat(list);
+          }));
+    }
+  }
+
+  addCourse(course: Course) {
+    if (course.selection !== null) {
+      if (course.selection === 'Instructor') {
+        this.profile.instructor.courses.push(course.key);
+        course.instructors.push(this.profile.key);
+      }
+      else {
+        this.profile.student.courses.push(course.key);
+        course.students.push(this.profile.key);
+      }
+
+      this.selectedCourses.push(course);
+
+      this.addedCourses.emit(this.selectedCourses);
+    }
+  }
+}
