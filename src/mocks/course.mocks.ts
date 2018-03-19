b1ec5f95a2877b9713ef8094aa229cce71d1dd3b
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-18 19:10:58 -0500

    Refactored names, changed interfaces and added folders

--- src/mocks/course.mocks.ts
+++ src/mocks/course.mocks.ts
@@ -5,25 +5,33 @@ const courseList: Course[] = [
     number: 'Comp225',
     title: 'Software Design',
     students: ['-L6mKXkPWGkzXvGeiIhh', '-L6mKXkUX52GIH7lmX5v', '-L6mKXkYvRO1D8dOoqxX'],
-    officeHours: []
+    instructors: [],
+    officeHours: [],
+    selection: ''
   },
   {
     number: 'Math236',
     title: 'Linear Algebra',
     students: ['-L6mKXkPWGkzXvGeiIhh', '-L6mKXkUX52GIH7lmX5v'],
-    officeHours: []
+    instructors: [],
+    officeHours: [],
+    selection: ''
   },
   {
     number: 'Comp221',
     title: 'Algorithm Design',
     students: ['-L6mKXkPWGkzXvGeiIhh', '-L6mKXkXCZjMfsK14X31'],
-    officeHours: []
+    instructors: [],
+    officeHours: [],
+    selection: ''
   },
   {
     number: 'Thda220',
     title: 'Physical Approaches',
     students: ['-L6mKXkPWGkzXvGeiIhh', '-L6mKXkXCZjMfsK14X31', '-L6mKXkYvRO1D8dOoqxX'],
-    officeHours: []
+    instructors: [],
+    officeHours: [],
+    selection: ''
   }
 ];
 
--- src/mocks/student.mocks.ts
+++ src/mocks/student.mocks.ts
@@ -2,19 +2,15 @@ import {Student} from "../models/student/student.interface";
 
 const studentList: Student[] = [
   {
-    name: 'Juan David',
     courses: ['-L6mMFzpDy8uZqOdi6aF', '-L6mMFzwauC-Exmi4oFv', '-L6mMFzzl-JLuEL-4tLG', '-L6mMG-2h_oLWXDdjRfs']
   },
   {
-    name: 'Salman',
     courses: ['-L6mMFzpDy8uZqOdi6aF', '-L6mMFzwauC-Exmi4oFv']
   },
   {
-    name: 'Raza',
     courses: ['-L6mMFzzl-JLuEL-4tLG', '-L6mMG-2h_oLWXDdjRfs']
   },
   {
-    name: 'David',
     courses: ['-L6mMFzpDy8uZqOdi6aF', '-L6mMG-2h_oLWXDdjRfs']
   },
 ];
