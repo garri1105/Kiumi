f6117e9ac86a23e40d8899b5439b3646688286e5
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-21 17:43:41 -0500

    Fixed merge conflicts
    Signed-off-by: Juan David Garrido <juandagarrido@hotmail.com>

--- src/components/edit-hours-form/edit-hours-form.ts
+++ src/components/edit-hours-form/edit-hours-form.ts
@@ -7,11 +7,7 @@ import { Component } from '@angular/core';
  * Components.
  */
 @Component({
-<<<<<<< HEAD
   selector: 'edit-hours-form',
-=======
-  selector: 'edit-hours',
->>>>>>> 287bbad38a5f7d1bfe98fb48fce08255be10826a
   templateUrl: 'edit-hours-form.html'
 })
 export class EditHoursComponent {
