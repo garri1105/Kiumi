982fc42ba10a308a8addb202256e0d1a5d70bebc
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-21 17:26:54 -0500

    EditProfile completed. When unchecking a box and saving the information is deleted from profile and courses
    Signed-off-by: Juan David Garrido <juandagarrido@hotmail.com>

--- src/components/edit-profile-form/edit-profile-form.ts
+++ src/components/edit-profile-form/edit-profile-form.ts
@@ -6,6 +6,9 @@ import {User} from "firebase/app";
 import {Instructor} from "../../models/instructor/instructor.interface";
 import {Student} from "../../models/student/student.interface";
 import {GlobalProfileProvider} from "../../providers/global-profile/global-profile";
+import {AlertController} from "ionic-angular";
+import {CourseDataProvider} from "../../providers/course-data/course-data";
+import {Course} from "../../models/course/course.interface";
 
 @Component({
   selector: 'edit-profile-form',
@@ -16,13 +19,14 @@ export class EditProfileFormComponent {
   authenticatedUser: User;
   profile: Profile;
   @Output() saveProfileResult: EventEmitter<Boolean>;
-
   instructorCheck: boolean;
   studentCheck: boolean;
 
   constructor(private profileData: ProfileDataProvider,
+              private courseData: CourseDataProvider,
               private auth: AuthProvider,
-              private globalProfile: GlobalProfileProvider) {
+              private globalProfile: GlobalProfileProvider,
+              private alert: AlertController) {
 
     this.profile = this.globalProfile.getProfile();
 
@@ -43,21 +47,89 @@ export class EditProfileFormComponent {
 
   saveProfile() {
     if (this.instructorCheck) {
-      this.profile.instructor = <Instructor> {
-        courses: ['0'],
-        officeHours: ['0']
-      };
+      if (!this.profile.instructor) {
+        this.profile.instructor = <Instructor> {
+          courses: ['0'],
+          officeHours: ['0']
+        };
+      }
+    }
+    else {
+      if (this.profile.instructor) {
+        this.profile.instructor.courses
+          .map(courseKey =>
+            this.courseData
+              .getCourseByKey(courseKey)
+              .valueChanges()
+              .subscribe((course: Course) => {
+                if (course) {
+                  let index = course.instructors.indexOf(this.profile.key);
+                  if (index > -1){
+                    course.instructors.splice(index, 1);
+                  }
+
+                  this.courseData.updateCourse(course);
+                }
+              })
+          );
+      }
+
+      this.profile.instructor = null;
     }
 
     if (this.studentCheck) {
-      this.profile.student = <Student> {
-        courses: ['0']
-      };
+      if (!this.profile.student) {
+        this.profile.student = <Student> {
+          courses: ['0']
+        };
+      }
+    }
+    else {
+      if (this.profile.student) {
+        this.profile.student.courses
+          .map(courseKey =>
+            this.courseData
+              .getCourseByKey(courseKey)
+              .valueChanges()
+              .subscribe((course: Course) => {
+                if (course) {
+                  let index = course.students.indexOf(this.profile.key);
+                  if (index > -1) {
+                    course.students.splice(index, 1);
+                  }
+                  this.courseData.updateCourse(course);
+                }
+              })
+          );
+
+        this.profile.student = null;
+      }
     }
 
     if (this.authenticatedUser) {
       this.profileData.saveProfile(this.authenticatedUser, this.profile)
         .then(value => this.saveProfileResult.emit(value));
+    }
+  }
+
+  alertUser(checkbox) {
+    if (!checkbox.valueAccessor.value) {
+      this.alert.create({
+        title: 'Are you sure?',
+        message: `Unchecking this box will delete your ${checkbox.name} information`,
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              checkbox.valueAccessor.writeValue(true);
+            }
+          },
+          {
+            text: 'Continue',
+          }
+        ]
+      }).present();
     }
   }
 
