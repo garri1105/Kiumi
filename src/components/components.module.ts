7e053b2b00fd5269d97b23baa07d0699202aa291
Juan David Garrido <juandagarrido@hotmail.com>
2018-03-20 20:27:59 -0500

    Fixed typo and removed auto-generated comments

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -5,7 +5,7 @@ import {LoginFormComponent} from "./login-form/login-form";
 import { CourseSearchComponent } from './course-search/course-search';
 import { EditProfileFormComponent } from './edit-profile-form/edit-profile-form';
 import { ProfileViewComponent } from './profile-view/profile-view';
-import { EditHoursComponent } from './edit-hours/edit-hours';
+import { EditHoursComponent } from "./edit-hours-form/edit-hours-form";
 
 @NgModule({
 	declarations: [CourseListComponent,
